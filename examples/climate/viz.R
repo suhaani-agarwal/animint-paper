afce98744ccc52fb0e2b372e98c3e9072cab2fe0
Suhaani Agarwal <suhaani0707@gmail.com>
2025-05-12 21:39:09 +0530

    made changes to climate data viz

--- examples/climate/viz.R
+++ examples/climate/viz.R
@@ -26,7 +26,7 @@ lims <- list(surftemp = c(-10, 40),
            surfdev = c(-8, 8))
 var.names <- c("surftemp", "surfdev")
 dot.alpha <- 6/10
-selected.color <- "#eb0deb"
+selected.color <- "#ff89ff"
 getlab <- function(var.name){
   sprintf("%s (degrees Celsius)", long.names[[var.name]])
 }
@@ -49,30 +49,13 @@ viz <- list(
         axis.text = element_blank(), 
         axis.ticks = element_blank(), 
         axis.title = element_blank()),
-  
-  # surfdevMap = ggplot() + 
-  #   theme_bw() +
-  #   geom_tile(aes(x = long, y = lat, fill = surfdev, key = id),
-  #           clickSelects = "id", showSelected = "time2",
-  #           data = climate) + 
-  #   scale_fill_gradient2("deg. C", low = "blue", mid = "white", high = "#ff0000",
-  #                      midpoint = 0, limits = lims$surfdev) + 
-  #   ggtitle("Deviation from monthly norm") +
-  #   geom_path(aes(long, lat, group = group), col = "#908f8f",
-  #           data = countries) + 
-  #   geom_text(aes(-86, 39, label = textdate,key = paste("date", time2)), showSelected = "time2",
-  #           data = dates) + 
-  #   theme(axis.line = element_blank(), 
-  #       axis.text = element_blank(), 
-  #       axis.ticks = element_blank(), 
-  #       axis.title = element_blank()),
   surfdevMap = ggplot() + 
   theme_bw() +
   geom_tile(aes(
     x = long, 
     y = lat, 
     fill = surfdev,
-    key = id  # Add key based on tile identifier
+    key = id
   ),
   clickSelects = "id", 
   showSelected = "time2",
@@ -106,33 +89,64 @@ viz <- list(
     ylim(-8, 8) +
     xlab(getlab(var.names[[1]])) +
     ylab(getlab(var.names[[2]])) +
-    geom_point(aes_string(x = var.names[[1]], y = var.names[[2]]),
+    geom_point(aes_string(x = var.names[[1]], y = var.names[[2]], key = "id"),
              clickSelects = "id", showSelected = "time2",
              data = climate, alpha = dot.alpha,
              selected.color = "black", selected.fill = selected.color),
-  
-  surftempTimeSeries = ggplot() +
-    theme_animint(width = 463) +
-    geom_hline(yintercept = 0) +
-    make_tallrect(climate, "time2") +
-    xlab("Year of measurement") +
-    ylab(getlab(var.names[[1]])) +
-    geom_line(aes_string(x = "time2", y = var.names[[1]],
-                         group = "id"), clickSelects = "id", color = "violet", alpha = 0.53,
-              data = climate,
-              selected.color = "violet", selected.fill = selected.color, color_off = "black", z_off = 0.1),
-  
+surftempTimeSeries = ggplot() +
+theme_bw()+
+  theme_animint(width = 463) +
+  geom_hline(yintercept = 0) +
+  make_tallrect(climate, "time2") +
+  xlab("Year of measurement") +
+  ylab(getlab(var.names[[1]])) +
+  geom_line(
+    aes_string(x = "time2", y = var.names[[1]], group = "id"),
+    data = climate,
+    colour = "black",
+    size = 1.5,
+    alpha = 0.55,
+    clickSelects = "id",
+    hover = list(
+      size = 2,
+      alpha = 1
+    )
+  ) +
+  geom_line(
+    aes_string(x = "time2", y = var.names[[1]], group = "id"),
+    data = climate,
+    colour = selected.color, # Purple for selected
+    size = 3,             # Slightly bolder when selected
+    alpha = 2,              # Opaque when selected
+    showSelected = "id"     # Only show for selected id
+  ),  # Actual selection,
   surfdevTimeSeries = ggplot() +
-    theme_animint(width = 463) +
-    geom_hline(yintercept = 0) +
-    make_tallrect(climate, "time2") +
-    xlab("Year of measurement") +
-    ylab(getlab(var.names[[2]])) +
-    geom_line(aes_string(x = "time2", y = var.names[[2]],
-                         group = "id"), clickSelects = "id", color = "violet", alpha = 0.53,
-              data = climate,
-              selected.color = "violet", selected.fill = selected.color, color_off = "black", z_index = 100),
-  
+  theme_bw()+
+  theme_animint(width = 463) +
+  geom_hline(yintercept = 0) +
+  make_tallrect(climate, "time2") +
+  xlab("Year of measurement") +
+  ylab(getlab(var.names[[2]])) +
+  geom_line(
+    aes_string(x = "time2", y = var.names[[2]], group = "id"),
+    data = climate,
+    colour = "black",
+    size = 1.5,
+    alpha = 0.55,
+    clickSelects = "id",
+    hover = list(
+      size = 2,
+      alpha = 1
+    )
+  ) +
+  geom_line(
+    aes_string(x = "time2", y = var.names[[2]], group = "id"),
+    data = climate,
+    colour = selected.color,
+    size = 2,
+    alpha = 2,
+    showSelected = "id"
+  ),  
   scatterHere = ggplot() +
     make_text(climate, 20, -7, "id", "all times for region %s") +
     theme_bw() +
@@ -142,7 +156,7 @@ viz <- list(
     geom_vline(xintercept = 0) +
     xlim(-10, 40) +
     ylim(-8, 8) +
-    geom_point(aes_string(x = var.names[[1]], y = var.names[[2]]), clickSelects = "time2", showSelected = "id",
+    geom_point(aes_string(x = var.names[[1]], y = var.names[[2]], key = "time2"), clickSelects = "time2", showSelected = "id",
              data = climate, alpha = dot.alpha,
              selected.color = "black", selected.fill = selected.color),
   
